77e3860b93d2eecc565da5ec3b6f6adc3e169dd8
marvin espira <espiramarvin@gmail.com>
2022-12-13 15:00:59 +0300

    feat: adds updates

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import * as SplashScreen from "expo-splash-screen"
+import AuthContextProvider, { AuthContext } from "./store/context/auth-context"
+import { useContext, useEffect, useState } from "react"
+import AppNavigation from "./navigations/AppNavigation"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+SplashScreen.preventAutoHideAsync()
+
+function Root() {
+  const [isTryingLoading, setIsTryingLoading] = useState(true)
+  const { setToken } = useContext(AuthContext)
+  useEffect(() => {
+    async function fetchToken() {
+      const storedToken = await AsyncStorage.getItem("react-native-auth-token")
+
+      if (storedToken) {
+        setToken(storedToken)
+      }
+
+      setIsTryingLoading(false)
+    }
+
+    fetchToken()
+  }, [])
+
+  if (!isTryingLoading) {
+    SplashScreen.hideAsync()
+  }
+
+  return <AppNavigation />
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+      <StatusBar style="light" backgroundColor="" />
+      <AuthContextProvider>
+        <Root />
+      </AuthContextProvider>
+    </>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
